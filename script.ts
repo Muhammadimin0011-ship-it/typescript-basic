0ff9a91d3e0d125f72cc796a0bf5f9e5afbb8d8b
Muhammadimin0011-ship-it <muhademinmamateminov@gmail.com>
2026-09-10 20:57:31 +0500

    added the new product

--- script.ts
+++ script.ts
@@ -9,3 +9,17 @@ function User({ name, age, isStudent }: User): void {
   console.log(age);
   console.log(isStudent);
 }
+
+const student: Student = {
+  name: "Muhammademin",
+  age: 15,
+  isStudent: true,
+  course: "frontend developer",
+};
+
+const product: Product = {
+  title: "iPhone 11 pro",
+  price: 200,
+  isAvailable: true,
+  category: "mobile" 
+}
